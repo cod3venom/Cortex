7918516339bcc14288deb851f4d8938da38ee79a
cod3venom <levan.ostrowski@gmail.com>
2020-12-17 21:25:10 +0100

    Removed unwanted

--- Cheater/AgencyFactory.py
+++
@@ -1,8 +0,0 @@
-from fake_useragent import UserAgent
-from Hacker.Logging import  Logging
-from Texts.Bundle import Bundle as __bundle__
-class AgencyFactory:
-    def __gen__(self):
-        agent = UserAgent(verify_ssl=False).random
-        Logging(1,__bundle__().getString(24), agent)
-        return agent
