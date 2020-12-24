c59972faedb8f76bd45a04b8a983b5e8baa5d278
cod3venom <levan.ostrowski@gmail.com>
2020-12-24 01:23:24 +0100

    fixed install.py by adding installConfig() function

--- Install.py
+++ Install.py
@@ -1,4 +1,6 @@
 import os, sys
+import shutil
+
 
 def pipinstall():
     with open('requirements.txt', 'r', encoding='utf8') as reader:
@@ -10,6 +12,9 @@ def pipinstall():
 def installChrome():
     os.system(f'dpkg -i {os.getcwd()}/Core/Browser/Bin/google-chrome.deb')
 
+def installConfig():
+    os.system('cp -r build/Cortex_config/* /usr/share/cortex')
+
 if __name__ == "__main__":
 
 
@@ -17,4 +22,7 @@ if __name__ == "__main__":
         installChrome()
     if sys.argv[1] == "install" and sys.argv[2] == "pipinstall":
         pipinstall()
+
+    if sys.argv[1] == "install" and sys.argv[2] == "config":
+        installConfig()
 
